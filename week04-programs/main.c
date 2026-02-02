62c4c22f0c81e051cd68c518b76711844316c6de
hstephan23 <harrison@stephanclan.com>
2026-02-02 10:16:36 -0700

    Week 4 work

--- week04-programs/main.c
+++ week04-programs/main.c
@@ -7,11 +7,39 @@
 // TODO:
 // Write a function that prints a divider
 // Write a function that adds two numbers
+float divide(int a, int b);
+int add_numbers(int a, int b);
+void print_divider(int a, int b, float result);
 
 int main(void) {
-  // TODO:
-  // Call functions
-  // Explain responsibility of each
+    // TODO:
+    // Call functions
+    // Explain responsibility of each
+    int a = 100;
+    int b = 33;
+    float result;
 
-  return 0;
+    // This is in charge of dividing
+    result = divide(a, b);
+    // this is in charge of adding
+    int result_of_addition = add_numbers(a, b);
+    // this is in charge of printing (Originally I did a divide and print, but that is not one responsibility)
+    print_divider(a, b, result);
+
+    return 0;
+}
+
+float divide(int a, int b)
+{
+    return (float)a / (float)b;
+}
+
+int add_numbers(int a, int b)
+{
+    return a + b;
+}
+
+void print_divider(int a, int b, float result)
+{
+    printf("%d divided by %d is %f\n", a, b, result);
 }
