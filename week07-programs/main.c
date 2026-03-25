a1fccfab6f245c05a40a2abd18fa5a28b3f99ec1
hstephan23 <129238385+hstephan23@users.noreply.github.com>
2026-03-25 13:29:26 -0600

    week07 (#10)
    * Week 05 work
    
    * Week 05 work - array extension
    
    * Week 06 work, with additionally problems
    
    * Week07 with practice problems around Structs

--- week07-programs/main.c
+++ week07-programs/main.c
@@ -5,18 +5,39 @@
 #include <stdio.h>
 
 struct Player {
-  int x;
-  int y;
-  int health;
+    int x;
+    int y;
+    int health;
 };
 
 int main(void) {
-  struct Player player;
+    struct Player player = {0};
 
-  // TODO:
-  // 1. Initialize
-  // 2. Modify
-  // 3. Explain grouping
+    player.x = 10;
+    player.y = 10;
+    player.health = 5;
 
-  return 0;
+    printf("Player data: \n"
+           "-------------\n"
+           "Player position: (%d, %d)\n"
+           "Player health: %d\n\n", player.x, player.y, player.health);
+
+    player.x--;
+    player.y--;
+    player.health--;
+
+    printf("Player data: \n"
+           "-------------\n"
+           "Player position: (%d, %d)\n"
+           "Player health: %d\n\n", player.x, player.y, player.health);
+    // TODO:
+    // 1. Initialize
+    // 2. Modify
+    // 3. Explain grouping
+
+    // grouping is a way to allow you to control and scope variables together.
+    // It allows you to be able to change things easier
+    // It allows for better traccking and visibility too of variables
+
+    return 0;
 }
