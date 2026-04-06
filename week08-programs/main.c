565521fda3a5871f25241c9a0eb443991abb5dfd
hstephan23 <harrison@stephanclan.com>
2026-04-06 15:43:26 -0600

    Week08 work

--- week08-programs/main.c
+++ week08-programs/main.c
@@ -5,13 +5,30 @@
 #include <stdio.h>
 
 int main(void) {
-  int values[] = {3, 1, 4, 1, 5};
-  int size = 5;
+    const int values[5] = {3, 1, 4, 1, 5};
+    const int size = 5;
+    int min = values[0];
+    // First -> start with the first value as your "smallest"
+    // Then -> compare with the next value
+    // IF smaller, replace
+    // ELSE continue
+    // Finally -> return that value
 
-  // TODO:
-  // 1. Write pseudocode
-  // 2. Implement smallest value
-  // 3. Explain complexity in English
+    for (int i = 1; i < size; i++)
+    {
+        if (min > values[i])
+        {
+            min = values[i];
+        }
+    }
 
-  return 0;
+    printf("value: %d\n", min);
+
+
+    // TODO:
+    // 1. Write pseudocode
+    // 2. Implement smallest value
+    // 3. Explain complexity in English
+
+    return 0;
 }
