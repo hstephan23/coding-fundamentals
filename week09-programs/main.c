d40bd0bfd7aec1ca7d640ab04f2e8304511a7d90
hstephan23 <129238385+hstephan23@users.noreply.github.com>
2026-04-13 11:18:45 -0600

    Week09 work (#12)

--- week09-programs/main.c
+++ week09-programs/main.c
@@ -3,11 +3,29 @@
 */
 
 #include "math_utils.h"
+#include <stdio.h>
 
 int main(void) {
-  // TODO:
-  // Use math utilities
-  // Explain dependency direction
+    // TODO:
+    // Use math utilities
+    // Explain dependency direction
+    int a = 12;
+    int b = 13;
+    int add, sub, multi;
+    double div;
+    add = addition(a, b);
+    sub = subtract(a, b);
+    multi = multiplication(a, b);
+    div = division(a, b);
 
-  return 0;
+    printf("add: %d \n"
+           "sub: %d \n"
+           "div: %f \n"
+           "multi: %d \n",
+           add,
+           sub,
+           div,
+           multi);
+
+    return 0;
 }
